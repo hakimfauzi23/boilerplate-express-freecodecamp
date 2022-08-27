73854639fdff19dab0f836f9495e2b4dbedd79aa
Hanif Fauzi Hakim <58885246+hakimfauzi23@users.noreply.github.com>
2022-08-27 03:50:27 +0000

    Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -26,6 +26,10 @@ app.get("/now", (req,res,next) => {
   next();
 },(req,res) => {
   res.json({time:req.time})
+});
+
+app.get("/:word/echo",(req,res) => {
+  res.json({echo:req.params.word});
 })
 
 
